041a0e52cdbce9169738c9ddb3413442170e09db
Yuvraj Sharma <yuvrajsharma21314@gmail.com>
2026-04-17 16:18:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", function (e) {
   const result = document.querySelector("#result");
 
   if (height === "" || height < 0 || isNaN(height)) {
-    result.innerHTML = `Please give a valid height ${height}`;
+    result.innerHTML = `Please give a valid height`;
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
     result.innerHTML = "Please give a valid weight";
   } else {
